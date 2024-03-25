b3bd77e26b9246f634e9dbcdfdca1b39b62c3018
EddyerDevv <urbinaramalazo@1986@gmail.com>
2024-03-24 18:50:17 -0600

    refactor(index.js): Code improvements and refactoring

--- src/index.js
+++ src/index.js
@@ -1,56 +1,62 @@
-require('@babel/register')({
-    presets: ['@babel/preset-env', '@babel/preset-react']
+require("@babel/register")({
+  presets: ["@babel/preset-env", "@babel/preset-react"],
 });
 
-const fs = require('fs');
-const path = require('path');
-const Reacto = require('react');
-const ReactDOMServer = require('react-dom/server');
+const fs = require("fs");
+const path = require("path");
+const React = require("react");
+const ReactDOMServer = require("react-dom/server");
 
-function TaumaluipasGet(jsx, name) {
-    name = name + ".jsx"
-    const jsxget = jsx.find(e => e === name);
-
-    if (jsxget) {
-        return jsxget;
-    } else {
-        throw new Error(`No se encontró el archivo "${name}"`);
-    }
+function findJSXFile(jsxFiles, name) {
+  const jsxFile = jsxFiles.find((file) => file === `${name}.jsx`);
+  return jsxFile ?? new Error(`File ${name}.jsx not found.`);
 }
 
-function view(dir, callback) {
-    fs.readdir(dir, (err, ar) => {
-        if (err) {
-            callback(err, null);
-            return;
+function getJSXFiles(dir, callback) {
+  fs.readdir(dir, (err, files) => {
+    if (err) return callback(err, null);
+
+    const jsxFiles = files.filter((file) => {
+      const filePath = path.join(dir, file);
+      return fs.statSync(filePath).isFile() && path.extname(file) === ".jsx";
+    });
+
+    callback(null, jsxFiles);
+  });
+}
+
+function configureReact(app, dir) {
+  app.use((req, res, next) => {
+    getJSXFiles(dir, (error, jsxFiles) => {
+      if (error) {
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      res.jsxFiles = jsxFiles;
+
+      res.renderReact = function (filename, props) {
+        const jsxFile = findJSXFile(jsxFiles, filename);
+        if (!jsxFile) {
+          res.status(404).send(`File ${filename} not found.`);
+          return;
         }
 
-        const JSX = ar.filter(e => {
-            const dirJSX = path.join(dir, e);
-            return fs.statSync(dirJSX).isFile() && path.extname(e) === '.jsx';
-        });
-        callback(null, JSX); 
+        const filePath = path.join(__dirname, "../../../", dir, jsxFile);
+        const Component = require(filePath).default;
+
+        const htmlString = ReactDOMServer.renderToString(
+          React.createElement(Component, props)
+        );
+
+        res.setHeader("Content-Type", "text/html");
+        htmlString.pipe(res);
+      };
+
+      next();
     });
+  });
 }
 
-function config(app, dir) {
-    app.use((req, res, next) => {
-        view(dir, (error, data) => {
-            if (error) {
-                console.error('Error:', error);
-                return;
-            }
-            res.jsxAr = data; 
-            res.react = function React(archivo, props) {
-                const bab = TaumaluipasGet(data, archivo);
-                const elePath = path.join(__dirname,"../../../", dir, bab);
-                const ele = require(elePath);
-                const html = ReactDOMServer.renderToString(Reacto.createElement(ele, props));
-                res.send(html);
-            };
-            next(); 
-        });
-    });
-}
-
-module.exports = { config }
+module.exports = { configureReact };
